73ab690abbd59f3f792304300e90e8aebb9f013a
JohnathanVidal <johnathanvidal1309@gmail.com>
2025-10-13 18:00:08 -0500

    actualizacion_del_home

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Box, Typography } from '@mui/material';
+import NubesFondo from '../../assets/nubes.jpg';
 
 const HomePage = () => {
     return (
@@ -8,28 +9,65 @@ const HomePage = () => {
                 p: 5,
                 textAlign: 'center',
                 color: 'white',
-                mt: 10,
-                minHeight: '60vh'
+                minHeight: '60vh',
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'center',
+                alignItems: 'center', 
+
+                backgroundImage: `url(${NubesFondo})`,
+                backgroundSize: 'cover',
+                backgroundPosition: 'center',
+                backgroundAttachment: 'fixed',
+
+                position: 'relative',
+                '&::before': {
+                    content: '""',
+                    position: 'absolute',
+                    top: 0,
+                    right: 0,
+                    bottom: 0,
+                    left: 0,
+                    backgroundColor: 'rgba(0, 0, 0, 0.25)',
+                    zIndex: 1,
+                    borderRadius: '4px'
+                },
             }}
         >
-            <Typography
-                variant="h1"
-                component="h1"
-                sx={{
-                    fontFamily: 'Bangers, cursive',
-                    color: '#FFCC00',
-                    textShadow: '5px 5px 0px #000000ff',
-                    mb: 3
-                }}
-            >
-                ¡BIENVENIDO A SPRINGFIELD!
-            </Typography>
-            <Typography
-                variant="h5"
-                sx={{ maxWidth: 800, margin: 'auto', textShadow: '2px 2px 0px #000000cc', color: '#e1e1e1ff' }}
-            >
-                Explora el universo de Los Simpson: personajes, lugares y episodios, todo consumido desde una API pública en React.
-            </Typography>
+            {/* El Box de contenido (zIndex: 2) es lo que se centra dentro del Box padre */}
+            <Box sx={{
+                position: 'relative',
+                zIndex: 2,
+                textAlign: 'center'
+            }}>
+                <Typography
+                    variant="h1"
+                    component="h1"
+                    sx={{
+                        fontFamily: 'Bangers, cursive',
+                        color: '#FFCC00',
+                        textShadow: '5px 5px 0px #000',
+                        mb: 3
+                    }}
+                >
+                    ¡BIENVENIDO A SPRINGFIELD!
+                </Typography>
+                <Typography
+                    variant="h5"
+                    sx={{
+                        maxWidth: 800,
+                        margin: 'auto',
+                        textShadow: '2px 2px 0px #000',
+                        color: '#FFF'
+                    }}
+                >
+                    En esta API podrás encontrar información detallada sobre los personajes, episodios y ubicaciones de la icónica serie de televisión llamada "Los Simpson". Puedes explorar datos interesantes, curiosidades y mucho más sobre la Familia Simpson y Springfield.
+                    <br />
+                    Esta API fue hecha usando los datos de la API publica de los simpson https://thesimpsonsapi.com/
+                    <br /><br />
+                    DISFRUTA DE TU VISITA!
+                </Typography>
+            </Box>
         </Box>
     );
 };
