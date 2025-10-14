80597be17fe1a38e45ff3264c02be11ab69a7dd4
JohnathanVidal <johnathanvidal1309@gmail.com>
2025-10-14 17:09:04 -0500

    fin

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+const repoName = '/ApiSimpsons_2025IIg1_Vidal_Johnathan/';
 export default defineConfig({
   plugins: [react()],
+  base: repoName,
 })
